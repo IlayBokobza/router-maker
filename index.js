b539fe7c1d94f4b36ee6f68b50b4f425442f15ac
Ilay Bokobza <kanoel200@gmail.com>
2020-11-23 19:20:34 +0200

    1.0

---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require('fs')
+const path = require('path')
+
+const options = require('./options')
+
+//gets all html files
+const views = fs.readdirSync(path.join(__dirname,options.input)).filter(file => {
+    const regEx = /\.html$/
+    return regEx.test(file)
+})
+let jsViewsFile = ""
+
+//gets to body from the html files and adds to to the file
+views.map(view => "const " + view.replace(/\.html$/,'') + " = `" + fs.readFileSync(path.join(__dirname,`${options.input}/${view}`)).toString()
+.replace(/\s/g,'')
+.match(/<body>(.*?)<\/body>/g,'').toString()
+.replace(/<body>|<\/body>/g,'')
++ "`;")
+.forEach(view => jsViewsFile += `${view}\n`)
+
+
+//makes routes array
+const routes = []
+views.forEach(view => {
+    view = view.replace(/\.html$/,'')
+
+    if(view === 'index'){
+        routes.push({
+            path:'/',
+            component:view  
+        })
+        return
+    }
+
+    routes.push({
+      path:`/${view}`,
+      component:view  
+    })
+})
+
+
+//adds the global component var
+jsViewsFile += "let component;\n"
+
+
+//adds the router
+jsViewsFile += `const setComponent = (path) => {
+    switch(path){
+`
+
+routes.forEach(route => {
+    jsViewsFile += "        case '" + route.path + "':\n"
+    jsViewsFile += `        component = ${route.component}\n`
+    jsViewsFile += "        break;\n\n"
+})
+
+jsViewsFile += "    }\n"
+jsViewsFile += "}\n"
+
+jsViewsFile += "const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';"
+
+//adds router logic
+jsViewsFile += `
+const router = () => {
+    const path = parseLocation();
+    setComponent(path)
+    document.getElementById('app').innerHTML = component;
+};
+`
+
+jsViewsFile += `
+window.addEventListener('hashchange', router);
+window.addEventListener('load', router);`
+
+
+//writes file
+fs.writeFileSync(path.join(__dirname,`${options.output}/router.js`),jsViewsFile)
---
+++ options.js
@@ -0,0 +1,18 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+let options = {
+    output:'./',
+    input:'./views'
+}
+
+if(argv.output){
+    options.output = argv.output
+}
+
+if(argv.input){
+    options.input = argv.input
+}
+
+module.exports = options
